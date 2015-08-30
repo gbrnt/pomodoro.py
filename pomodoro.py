c71adfbe09f7a3db617f39666f00ac6960e25dc1
George Bryant <gb@gbryant.co.uk>
2015-08-30 15:18:07 +0100

    Initialise pomdoro.py

---
+++ pomodoro.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+"""
+Pomodoro.py - Utility to time and track pomodoros
+
+The app on my tablet works well, but it'd be nice to have the information in a database I can access after a week of intensive work to get some statistics from it.
+"""
